833b1c9fcf9a934532a33891cebd826d61acb0d3
Daniel Spencer <nerdromancer@gmail.com>
2018-06-06 23:41:37 -0700

    Updated framework

--- PBFrameworkUILibraries.framework/Headers/PBFrameworkUILibraries.h
+++ PBFrameworkUILibraries.framework/Headers/PBFrameworkUILibraries.h
@@ -14,6 +14,51 @@ FOUNDATION_EXPORT double PBFrameworkUILibrariesVersionNumber;
 //! Project version string for PBFrameworkUILibraries.
 FOUNDATION_EXPORT const unsigned char PBFrameworkUILibrariesVersionString[];
 
-#import <PBFrameworkUILibraries/PBFrameworkUILibraries-umbrella.h>
-
-
+#import <PBFrameworkUILibraries/TPPropertyAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallClipRotateMultipleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallGridBeatAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorLineScalePartyAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallZigZagDeflectAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorTwoDotsAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallZigZagAnimation.h>;
+#import <PBFrameworkUILibraries/CERoundProgressLayer.h>;
+#import <PBFrameworkUILibraries/CERoundProgressView.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorAnimationProtocol.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorTripleRingsAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorRotatingSandglassAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallRotateAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallClipRotatePulseAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorRotatingSquaresAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorLineScalePulseOutAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorFiveDotsAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorDoubleBounceAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallScaleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorLineScalePulseOutRapidAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorTriangleSkewSpinAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorNineDotsAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallGridPulseAnimation.h>;
+#import <PBFrameworkUILibraries/VideoRecordTimer.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallTrianglePathAnimation.h>;
+#import <PBFrameworkUILibraries/FocusSquare.h>;
+#import <PBFrameworkUILibraries/OverlayPosePopup.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallBeatAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorCookieTerminatorAnimation.h>;
+#import <PBFrameworkUILibraries/iCarousel.h>;
+#import <PBFrameworkUILibraries/JPSImagePickerController.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorLineScaleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorView.h>;
+#import <PBFrameworkUILibraries/JPSVolumeButtonHandler.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorThreeDotsAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallScaleRippleMultipleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallClipRotateAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallScaleMultipleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallPulseSyncAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallPulseAnimation.h>;
+#import <PBFrameworkUILibraries/JPSSettings.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorTriplePulseAnimation.h>;
+#import <PBFrameworkUILibraries/UIAlertController+Window.h>;
+#import <PBFrameworkUILibraries/JPSCameraButton.h>;
+#import <PBFrameworkUILibraries/UIImage+DrawBlock.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorBallScaleRippleAnimation.h>;
+#import <PBFrameworkUILibraries/DGActivityIndicatorRotatingTrigonAnimation.h>;
+#import <PBFrameworkUILibraries/KAProgressLabel.h>;
